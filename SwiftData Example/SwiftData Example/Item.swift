63000248e292da6095530e6385cd884a536e9999
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-06-30 17:21:33 +0530

    initial setup

---
+++ SwiftData Example/SwiftData Example/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftData Example
+//
+//  Created by Siddharth Kothari on 30/06/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
