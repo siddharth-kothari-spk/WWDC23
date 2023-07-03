d5380f980e2091d65f455549a9e94eb7a6f91df3
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-03 16:50:39 +0530

    initial setup for chart

---
+++ Charts Sample/Charts Sample/Charts_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_SampleApp.swift
+//  Charts Sample
+//
+//  Created by Siddharth Kothari on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Charts_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
