5e6319efb7c92e347fdfe9e7d2aa1061c734ad73
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-03 18:01:20 +0530

    vertical bar chart added

--- Charts Sample/Charts Sample/ContentView.swift
+++ Charts Sample/Charts Sample/ContentView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    
+    private var coffeeSales = [
+        (name: "Americano", count: 120),
+        (name: "Cappuccino", count: 234),
+        (name: "Espresso", count: 62),
+        (name: "Latte", count: 625),
+        (name: "Mocha", count: 320),
+        (name: "Affogato", count: 50)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart {
+                ForEach(coffeeSales, id: \.name) { coffee in
+                    BarMark(
+                        x: .value("Type", coffee.name),
+                        y: .value("Cup", coffee.count)
+                    )
+                    .foregroundStyle(by: .value("Type", coffee.name))
+                    //.clipShape(Circle())
+                }
+            }
         }
         .padding()
     }
