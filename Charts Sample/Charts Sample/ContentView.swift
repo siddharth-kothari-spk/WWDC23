b78374f851572144736f81398dbdbeda884e075a
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-04 14:34:29 +0530

    added 1. x axis , 2. y axis, 3. interval charts

--- Charts Sample/Charts Sample/ContentView.swift
+++ Charts Sample/Charts Sample/ContentView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 import Charts
 
+
+struct Profit {
+     let department: String
+     let profit: Double
+ }
+    
+struct Job {
+     let job: String
+     let start: Double
+     let end: Double
+ }
+
 struct ContentView: View {
     
     private var coffeeSales = [
@@ -17,6 +29,21 @@ struct ContentView: View {
         (name: "Latte", count: 625),
         (name: "Mocha", count: 320),
         (name: "Affogato", count: 50)
+    ]
+    
+    private var profitData: [Profit] = [
+        Profit(department: "Production", profit: 15000),
+        Profit(department: "Marketing", profit: 8000),
+        Profit(department: "Finance", profit: 10000)
+   ]
+    
+    private var jobData: [Job] = [
+        Job(job: "Job 1", start: 0, end: 15),
+        Job(job: "Job 2", start: 5, end: 25),
+        Job(job: "Job 1", start: 20, end: 35),
+        Job(job: "Job 1", start: 40, end: 55),
+        Job(job: "Job 2", start: 30, end: 60),
+        Job(job: "Job 2", start: 30, end: 60)
     ]
     
     var body: some View {
@@ -37,6 +64,55 @@ struct ContentView: View {
                 //Image(systemName: "house.fill")
                 Text("Vertical Bar")
             }
+            
+            // only x axis:
+            Chart {
+                ForEach(profitData, id: \.department) { profit in
+                    BarMark(
+                        x: .value("Cup", profit.profit)
+                    )
+                    .foregroundStyle(by: .value("Type", profit.department))
+                    //.clipShape(Circle())
+                }
+            }.frame(width: 400, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .tabItem {
+                //Image(systemName: "house.fill")
+                Text("X axis")
+            }
+            
+            // only y axis:
+            Chart {
+                ForEach(profitData, id: \.department) { profit in
+                    BarMark(
+                        y: .value("Cup", profit.profit)
+                    )
+                    .foregroundStyle(by: .value("Type", profit.department))
+                    //.clipShape(Circle())
+                }
+            }.frame(width: 300, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .tabItem {
+                //Image(systemName: "house.fill")
+                Text("Y axis")
+            }
+
+            
+            // Interval chart:
+            Chart {
+                ForEach(jobData, id: \.job) { job in
+                    BarMark(
+                         xStart: .value("Start Time", job.start),
+                        xEnd: .value("End Time", job.end),
+                         y: .value("Job", job.job)
+                             )
+                    .foregroundStyle(by: .value("Type", job.job))
+                    //.clipShape(Circle())
+                }
+            }
+            .tabItem {
+                //Image(systemName: "house.fill")
+                Text("Interval chart")
+            }
+            
         }
         .padding()
     }
