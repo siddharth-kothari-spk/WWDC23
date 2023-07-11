80c59114e432b9bfa400ff1b4ce31d6a1a731d89
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-07-11 16:09:51 +0530

    Pie Chart setup

---
+++ PieChart/PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Siddharth Kothari on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
