a2e988173dbd34b15a48c871b1dd7575d7c9383e
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-02 19:39:30 +0530

    exploring mapkit - initial commit

---
+++ MapKitSample/MapKitSample/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  MapKitSample
+//
+//  Created by Siddharth Kothari on 02/09/23.
+//
+
+// courtsey: https://www.polpiella.dev/mapkit-and-swiftui-searchable-map/
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitSample/MapKitSample/MapKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSampleApp.swift
+//  MapKitSample
+//
+//  Created by Siddharth Kothari on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
