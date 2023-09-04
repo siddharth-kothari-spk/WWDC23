15ee67860fade93b3d4e88e1329ec9c776f56947
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-04 09:01:00 +0530

    search completions model added

---
+++ MapKitSample/MapKitSample/Models/SearchCompletions.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCompletions.swift
+//  MapKitSample
+//
+//  Created by Siddharth Kothari on 04/09/23.
+//
+
+import Foundation
+
+struct SearchCompletions: Identifiable {
+    let id = UUID()
+    let title: String
+    let subTitle: String
+}
+
