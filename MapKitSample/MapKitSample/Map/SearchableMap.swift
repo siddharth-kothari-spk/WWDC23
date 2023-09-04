05505dce78b856939266ff215440a96f917deb9c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-04 16:42:24 +0530

    look-around scenes - need to retrieve a scene from a coordinate and pass it through to the new view

--- MapKitSample/MapKitSample/Map/SearchableMap.swift
+++ MapKitSample/MapKitSample/Map/SearchableMap.swift
@@ -15,6 +15,8 @@ struct SearchableMap: View {
     @State private var searchResults = [SearchResults]() // property to hold the search results
     @State private var selectedLocation: SearchResults? // property to hold the selected location.
     
+    @State private var scene: MKLookAroundScene? // A utility class that encapsulates information the framework requires to retrieve and display a specific Look Around location’s imagery.
+    
     var body: some View {
         Map(position: $position, selection: $selectedLocation) {
             ForEach(searchResults) { result in
@@ -24,8 +26,22 @@ struct SearchableMap: View {
                 .tag(result) // // Marker view to show each search result on the map
             }
         }
+        .overlay(alignment: .bottom, content: {
+            if selectedLocation != nil {
+                LookAroundPreview(scene: $scene, allowsNavigation: false, showsRoadLabels: true, pointsOfInterest: .including([.bank, .atm, .bakery, .brewery]), badgePosition: .bottomTrailing)
+                    .frame(height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .safeAreaPadding(.bottom, 40)
+                    .padding(.horizontal, 20)
+            }
+        })
             .ignoresSafeArea() // modifier to make the map view extend to the top and bottom of the screen
             .onChange(of: selectedLocation) {
+                if let selectedLocation {
+                    Task {
+                        scene = try? await fetchScene(for: selectedLocation.location)
+                    }
+                }
                 isSheetPresented = selectedLocation == nil
                 // update the SearchableMap view to dismiss the sheet when a location is selected
             }
@@ -39,6 +55,11 @@ struct SearchableMap: View {
                 SheetView(searchResults: $searchResults)
             }) // add a sheet overlay with a custom view that allows the user to search for locations
     }
+    
+    private func fetchScene(for coordinate: CLLocationCoordinate2D) async throws -> MKLookAroundScene? {
+            let lookAroundScene = MKLookAroundSceneRequest(coordinate: coordinate)
+            return try await lookAroundScene.scene
+        }
 }
 
 #Preview {
