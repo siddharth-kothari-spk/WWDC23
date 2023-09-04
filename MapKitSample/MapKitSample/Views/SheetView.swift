5b9291b6f9bb645d753c3eee29c569f51bee5483
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-04 16:21:31 +0530

    Displaying search results

--- MapKitSample/MapKitSample/Views/SheetView.swift
+++ MapKitSample/MapKitSample/Views/SheetView.swift
@@ -12,18 +12,28 @@ struct SheetView: View {
     @State private var locationService = LocationService(completer: .init()) // new state property to hold the location service instance
     @State private var search: String = ""
     
+    @Binding var searchResults: [SearchResults] // property to update the search results. This property will be held as a @State property in the parent view so that the map can be updated accordingly.
+    
     var body: some View {
         VStack {
             HStack(content: {
                 Image(systemName: "magnifyingglass")
                 TextField("Search for a restaurant", text: $search)
                     .autocorrectionDisabled()
+                    .onSubmit {
+                        // onSubmit modifier on the text field to trigger a search when the user presses the return key.
+                        Task {
+                            searchResults = (try? await locationService.search(with: search)) ?? []
+                        }
+                    }
             }).modifier(TextFieldGrayBackgroundColor())// A modifier that you apply to a view or another view modifier, producing a different version of the original value.
             Spacer()
             
             List {
                 ForEach(locationService.completions) { completion in
-                    Button(action: {}) {
+                    Button(action: {
+                        didTapOnCompletion(completion) // method to trigger a search when the user taps on a completion result.
+                    }) {
                         VStack(alignment: .leading, spacing: 4.0, content: {
                             Text(completion.title)
                                 .font(.headline)
@@ -51,6 +61,14 @@ struct SheetView: View {
         // ShapeStyle: A color or pattern to use when rendering a shape.
         .presentationBackgroundInteraction(.enabled(upThrough: .large)) // sheet view's background is interactive, meaning that the user can interact with the map view behind it
         // PresentationBackgroundInteraction: The kinds of interaction available to views behind a presentation.
+    }
+    
+    private func didTapOnCompletion(_ completion: SearchCompletions) { // method to search for the location's title and subtitle when the user taps on a completion result
+        Task {
+            if let singleLocation = try? await locationService.search(with: "\(completion.title) \(completion.subTitle)").first {
+                searchResults = [singleLocation]
+            }
+        }
     }
 }
 
