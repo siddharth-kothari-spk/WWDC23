3c6abf6c117558e1245525bbb6e4272f0018a6f3
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-04 09:06:56 +0530

    Location service initialized

--- MapKitSample/MapKitSample/Location/LocationService.swift
+++ MapKitSample/MapKitSample/Location/LocationService.swift
@@ -8,3 +8,26 @@
 import Foundation
 import MapKit
 
+
+@Observable
+class LocationService: NSObject {
+    private let completer: MKLocalSearchCompleter
+    
+    var completions = [SearchCompletions]()
+    
+    init(completer: MKLocalSearchCompleter) {
+        self.completer = completer
+        super.init()
+        self.completer.delegate = self
+    }
+}
+
+extension LocationService: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        
+    }
+}
